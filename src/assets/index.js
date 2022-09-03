f8320652f798780bec2a70d360880d7fcfd744ef
Ahmad Jafari <me@jafari.dev>
2022-09-03 22:54:42 +0430

    Implment Search component

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,3 @@
 export { ReactComponent as Sun } from "./sun.svg";
 export { ReactComponent as Moon } from "./moon.svg";
+export { ReactComponent as Search } from "./search.svg";
---
+++ src/components/Search/index.jsx
@@ -0,0 +1,23 @@
+import "./styles.scss";
+import { memo, useCallback } from "react";
+import { Search as SearchIcon } from "../../assets";
+
+function Search({searchValue, onChange}) {
+  const handleChangeValue = useCallback((event) => {
+    const value = event.currentTarget.value;
+    onChange(value);
+  }, [onChange]);
+
+  return (
+    <div className="search">
+      <SearchIcon />
+      <input
+        placeholder="Search for a country ..."
+        value={searchValue}
+        onChange={handleChangeValue}
+      />
+    </div>
+  )
+}
+
+export default memo(Search);
