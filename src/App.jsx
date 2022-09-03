865e1127acd87d70c0c6673ad338a0f363e1d7f6
Ahmad Jafari <me@jafari.dev>
2022-09-03 22:14:09 +0430

    Fetch the countries from api and show them in the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
+import { useCallback, useEffect, useState } from "react";
+import { CountryCard, Header } from "./components";
+
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [isDarkThemeOn, setIsDarkThemeOn] = useState(false);
+  const toggleTheme = useCallback(() => {
+    setIsDarkThemeOn((previousState) => !previousState);
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      const URL = "https://restcountries.com/v3.1/all";
+
+      try {
+        const response = fetch(URL);
+        const data = await (await response).json();
+
+        setCountries(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <h1>Where in The Worls?</h1>
+    <div className="container">
+      <Header
+        isDarkThemeOn={isDarkThemeOn}
+        onToggleTheme={toggleTheme}
+      />
+      <section className="countries">
+        {countries.map((country) => (
+          <CountryCard
+            key={country.name.common}
+            name={country.name.common}
+            capital={country.capital}
+            population={country.population}
+            region={country.region}
+            flagUrl={country.flags.svg}
+          />
+        ))}
+      </section>
     </div>
   );
 }
